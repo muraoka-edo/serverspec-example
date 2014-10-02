9bcf32d0d5e8c1c15bb1ada7b52fa3714c61a84f
エドアルド <edo@mac-3.local>
2014-10-02 11:55:19 +0900

    update: json genarator modify

--- utils/generate_json_properties.rb
+++ utils/generate_json_properties.rb
@@ -4,7 +4,7 @@
 require 'json'
 require 'csv'
 
-class GenProps
+class GenarateJsonFomatter
   def initialize(hostlst)
     @hostlst  = hostlst
     @properties = Array.new
@@ -20,15 +20,15 @@ class GenProps
       hostname  = row["hostname"]
       env       = row["env"     ]
       user      = row["user"    ]
-      attrs     = row["attrs"   ].split(':')
-      features  = row["features"].split(':')
+      roles     = row["roles"   ].split(':')
+#      features  = row["features"].split(':')
       property_of = Hash.new
       property_of[:host    ] = host
       property_of[:hostname] = hostname
       property_of[:env     ] = env
       property_of[:user    ] = user
-      property_of[:roles   ] = attrs
-      property_of[:features] = features
+      property_of[:roles   ] = roles
+#      property_of[:features] = features
       @properties << property_of
     end
 
@@ -39,3 +39,7 @@ class GenProps
     JSON.pretty_generate(@properties)
   end
 end
+
+# exec
+raise 'ArgumentError: Set Filename' if ARGV[0].nil?
+GenarateJsonFomatter.new(ARGV[0])
